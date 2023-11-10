3a8331b2ec7ad271859ca9dcfa54b58010d201c0
khalid harbi <ka901od@gmail.com>
2023-11-11 02:24:07 +0300

    commit

--- Strings.js
+++ Strings.js
@@ -1,4 +1,10 @@
-let language = 'JavaScript';
+/*
+et language = 'JavaScript';
 let message = 'Welcome to ';
 
 console.log(message + language);
+*/
+
+let language = 'JavaScript';
+
+console.log(language[0]);
