92a531cb8d5c06407f4cf60387255a100a05dc60
khalid harbi <ka901od@gmail.com>
2023-11-11 02:46:51 +0300

    commit

--- Arrays.js
+++ Arrays.js
@@ -5,3 +5,15 @@ colors[1] = 'black';
 console.log(colors);
 console.log(colors.length);
 console.log(colors.includes('red'));
+colors.push('green');
+console.log(colors);
+colors.push('gray');
+console.log(colors);
+colors.pop();
+console.log(colors);
+colors.unshift('gray');
+console.log(colors);
+
+const graycolor = colors.shift();
+console.log(colors);
+console.log(graycolor);
