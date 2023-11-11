7db5050dd816aba78a3ce8fb685f73d6faacb8d0
khalid harbi <ka901od@gmail.com>
2023-11-11 03:05:01 +0300

    commit

--- Arrays.js
+++ Arrays.js
@@ -32,3 +32,6 @@ console.log(values[2][1]);
 let message = 'Welcoome to JavaScript';
 let result = message.split(' ');
 console.log(result);
+
+let newMess = result.join('@');
+console.log(newMess);
