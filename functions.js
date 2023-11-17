da243b610a35e8eeb68aedac6409fdd16b816c41
khalid harbi <ka901od@gmail.com>
2023-11-17 19:23:04 +0300

    init

---
+++ functions.js
@@ -0,0 +1,30 @@
+/*
+function printNum(){
+    console.log(0);
+}
+
+printNum();
+
+function printNumbers(to){
+    for(let i =1; i<= to; i++){
+        console.log(i);
+    }
+}
+
+printNumbers(8);
+
+
+function add(firstNum, secondNumber){
+    console.log(firstNum + secondNumber);
+}
+
+add(2,7);
+*/
+
+function add(firstNum, seceondNum){
+    let result = firstNum + seceondNum;
+    return result;
+}
+
+let value = add(2, 7);
+console.log(value);
