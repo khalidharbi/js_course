e7d258b9ad9a1d31f9ae677863f2dd0580938495
khalid harbi <ka901od@gmail.com>
2023-11-17 03:43:31 +0300

    init

--- if.js
+++ if.js
@@ -13,3 +13,12 @@ console.log("Sorry, you are not old enough to play this game");
 else{
 console.log("Welcome to the game");  
 }
+
+let number = 1;
+if (number > 0) {
+console.log('+');
+}else if (number < 0) {
+console.log('-');
+}else {
+console.log('Zero');
+}
