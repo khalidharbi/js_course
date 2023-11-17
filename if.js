5fc38873e7a3656d06788f8d7926378c6defef87
khalid harbi <ka901od@gmail.com>
2023-11-17 03:51:20 +0300

    init

--- if.js
+++ if.js
@@ -6,6 +6,7 @@
 //console.log('Sorry, you are not old enough to play this game.');
 //};
 
+/*
 let age = 14;
 if (age < 12){
 console.log("Sorry, you are not old enough to play this game");
@@ -21,4 +22,20 @@ console.log('+');
 console.log('-');
 }else {
 console.log('Zero');
+}
+*/
+
+const colors = ['red', 'green', 'blue'];
+let colorNum = 1;
+
+switch(colorNum){
+    case 1:
+        console.log(colors[0]);
+        break;
+    case 2: 
+        console.log(colors[1]);
+        break;
+    case 3:
+        console.log(colors[2]);
+        break;
 }
